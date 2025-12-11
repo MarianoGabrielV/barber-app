073e6de92fb51b7733a3e36cb09203456f95b02d
MarianoVergniaud <mariano.unla@gmail.com>
2025-12-11 14:28:32 -0300

    aaa

--- src/config/clientConfig.js
+++ src/config/clientConfig.js
@@ -2,7 +2,7 @@
 export const clientConfig = {
   // Cambiá este número por el de la barbería (sin +, sin espacios)
   // Ejemplo: 54 (Argentina) + 9 (cel) + 11 (caba) + numero
-  phone: "5491112345678",
+  phone: "+5491162123307",
   shopName: "GoStyle Rulibarber",
   address: "Av. Siempreviva 123, Buenos Aires",
 };
